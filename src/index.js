d563aee403c17d1aee8400486521d0662e922cf9
camzach <cameron.zach@temple.edu>
2019-10-21 17:18:18 -0400

    Portfix (#2)
    * Add beginnings of node API
    
    * More work on GeoJSON
    
    * Working Node API
    
    * Use ENV vars

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ db.once('open', function() {
     // we're connected!
 });
 
+const mongo_uri = process.env.MONGODB_URI
+
+const mongoose = require('mongoose');
+mongoose.connect(mongo_uri, {useNewUrlParser: true});
+
+const geodata = require('./routes/geodata')(mongoose);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use('/geodata', geodata);
